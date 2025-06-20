988e6d05fbd4887c1c825d54da13a348bec3ffc5
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2025-06-20 11:05:37 -0500

    DB-04 - Enhance typing signatures

--- src/mongo_db/pokedex_mongo_client.py
+++ src/mongo_db/pokedex_mongo_client.py
@@ -1,16 +1,19 @@
 from mongo_db.models.pokemon import Pokemon
 
 from pymongo import AsyncMongoClient
-from pymongo.collection import Collection
+from pymongo.asynchronous.collection import AsyncCollection
 
 
 class PokedexMongoClient:
-    def __init__(self, uri: str) -> None:
-        client = AsyncMongoClient(uri)
+    def __init__(self, uri: str | None) -> None:
+        if uri is None:
+            raise ValueError("Database URI must be provided")
+
+        client: AsyncMongoClient = AsyncMongoClient(uri)
         self.client = client
 
         database = client.get_database("pokedex")
-        self._pokemon: Collection[Pokemon] = database.get_collection("pokemon")
+        self._pokemon: AsyncCollection[Pokemon] = database.get_collection("pokemon")
 
     async def get_pokemon_by_id(self, id: int) -> Pokemon | None:
         query = {"id": id}
